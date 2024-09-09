8840eec7a536be87184b05236b0e7ed4cc02e70e
fuxingyy <itsfuxingyau@gmail.com>
2024-09-08 23:30:12 -0500

    Add files via upload

---
+++ js/galeriacat.js
@@ -0,0 +1,58 @@
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Add an array to store image details
+var images = [
+  { id: 'cat1', alt: '1' },
+  { id: 'cat2', alt: '2' },
+  { id: 'cat3', alt: '3' },
+  { id: 'cat4', alt: '4' },
+  { id: 'cat5', alt: '5' },
+  { id: 'cat6', alt: '6' },
+  { id: 'cat7', alt: '7' }
+  
+];
+
+// Initialize the current slide index
+var currentSlide = 0;
+
+// Function to display the modal with the specified image
+function openModal(index) {
+  modal.style.display = "block";
+  modalImg.src = document.getElementById(images[index].id).src;
+  captionText.innerHTML = images[index].alt;
+  currentSlide = index;
+}
+
+// Function to close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Function to navigate to the next or previous slide
+function plusSlides(n) {
+  currentSlide += n;
+  if (currentSlide < 0) {
+    currentSlide = images.length - 1;
+  } else if (currentSlide >= images.length) {
+    currentSlide = 0;
+  }
+  openModal(currentSlide);
+}
+
+// Attach click event listeners to each image
+images.forEach(function(image, index) {
+  var img = document.getElementById(image.id);
+  img.onclick = function() {
+    openModal(index);
+  };
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  closeModal();
+}
---
+++ js/galeriacisa.js
@@ -0,0 +1,80 @@
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Add an array to store image details
+var images = [
+  { id: 'cisa1', alt: '1' },
+  { id: 'cisa2', alt: '2' },
+  { id: 'cisa3', alt: '3' },
+  { id: 'cisa4', alt: '4' },
+  { id: 'cisa5', alt: '5' },
+  { id: 'cisa6', alt: '6' },
+  { id: 'cisa7', alt: '7' },
+  { id: 'cisa8', alt: '8' },
+  { id: 'cisa9', alt: '9' },
+  { id: 'cisa10', alt: '10' },
+  { id: 'cisa11', alt: '11' },
+  { id: 'cisa12', alt: '12' },
+  { id: 'cisa13', alt: '13' },
+  { id: 'cisa14', alt: '14' },
+  { id: 'cisa15', alt: '15' },
+  { id: 'cisa16', alt: '16' },
+  { id: 'cisa17', alt: '17' },
+  { id: 'cisa18', alt: '18' },
+  { id: 'cisa19', alt: '19' },
+  { id: 'cisa20', alt: '20' },
+  { id: 'cisa21', alt: '21' },
+  { id: 'cisa22', alt: '22' },
+  { id: 'cisa23', alt: '23' },
+  { id: 'cisa24', alt: '24' },
+  { id: 'cisa25', alt: '25' },
+  { id: 'cisa26', alt: '26' },
+  { id: 'cisa27', alt: '27' },
+  { id: 'cisa28', alt: '28' },
+  { id: 'cisa29', alt: '29' }
+  
+];
+
+// Initialize the current slide index
+var currentSlide = 0;
+
+// Function to display the modal with the specified image
+function openModal(index) {
+  modal.style.display = "block";
+  modalImg.src = document.getElementById(images[index].id).src;
+  captionText.innerHTML = images[index].alt;
+  currentSlide = index;
+}
+
+// Function to close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Function to navigate to the next or previous slide
+function plusSlides(n) {
+  currentSlide += n;
+  if (currentSlide < 0) {
+    currentSlide = images.length - 1;
+  } else if (currentSlide >= images.length) {
+    currentSlide = 0;
+  }
+  openModal(currentSlide);
+}
+
+// Attach click event listeners to each image
+images.forEach(function(image, index) {
+  var img = document.getElementById(image.id);
+  img.onclick = function() {
+    openModal(index);
+  };
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  closeModal();
+}
