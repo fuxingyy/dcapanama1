edafe96514b8964c74babddd839b5bf401c1562c
fuxingyy <itsfuxingyau@gmail.com>
2025-04-09 09:56:55 -0500

    Create galeriayiad.js

---
+++ js/galeriayiad.js
@@ -0,0 +1,61 @@
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Add an array to store image details
+var images = [
+  { id: 'yiad1', alt: '1' },
+  { id: 'yiad2', alt: '2' },
+  { id: 'yiad3', alt: '3' },
+  { id: 'yiad4', alt: '4' },
+  { id: 'yiad5', alt: '5' },
+  { id: 'yiad6', alt: '6' },
+  { id: 'yiad7', alt: '7' },
+  { id: 'yiad8', alt: '8' },
+  { id: 'yiad9', alt: '9' },
+  { id: 'yiad10', alt: '10' }
+  
+];
+
+// Initialize the current slide index
+var currentSlide = 0;
+
+// Function to display the modal with the specified image
+function openModal(index) {
+  modal.style.display = "block";
+  modalImg.src = document.getElementById(images[index].id).src;
+  captionText.innerHTML = images[index].alt;
+  currentSlide = index;
+}
+
+// Function to close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Function to navigate to the next or previous slide
+function plusSlides(n) {
+  currentSlide += n;
+  if (currentSlide < 0) {
+    currentSlide = images.length - 1;
+  } else if (currentSlide >= images.length) {
+    currentSlide = 0;
+  }
+  openModal(currentSlide);
+}
+
+// Attach click event listeners to each image
+images.forEach(function(image, index) {
+  var img = document.getElementById(image.id);
+  img.onclick = function() {
+    openModal(index);
+  };
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  closeModal();
+}
